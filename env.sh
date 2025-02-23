8c1f66054ebf1fc97420b500cabb0a8f21f5e7d9
Rui Zhang <keen_1999@163.com>
2025-02-23 15:27:57 +0800

    add env.sh

---
+++ env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# check the necessary tools
+if ! command -v bison > /dev/null 2>&1; then
+    echo "bison is not installed. Installing..."
+    sudo apt update
+    sudo apt install bison
+else
+    echo "bison is installed."
+fi
+
+if ! command -v flex > /dev/null 2>&1; then
+    echo "flex is not installed. Installing..."
+    sudo apt update
+    sudo apt install flex
+else
+    echo "flex is installed."
+fi
+
+if dpkg -l | grep -q libncurses-dev; then
+    echo "libncurses-dev is installed."
+else
+    echo "libncurses-dev is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y libncurses-dev
+fi
+
+if dpkg -l | grep -q libreadline-dev; then
+    echo "libreadline-dev is installed."
+else
+    echo "libreadline-dev is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y libreadline-dev
+fi
+
+# set enviroment
+export NEMU_HOME=$(pwd)
