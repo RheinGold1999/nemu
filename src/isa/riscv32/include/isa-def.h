730a2a67111b16865089d3b1e8e44ae7e921055c
Rui Zhang <rui.zhang@aigcic.com>
2025-04-02 20:25:49 +0800

    add first intr

--- src/isa/riscv32/include/isa-def.h
+++ src/isa/riscv32/include/isa-def.h
@@ -18,9 +18,16 @@
 
 #include <common.h>
 
+#define CPU_CSR_SIZE (4096)
+#define CPU_CSR_MSTATUS (0x300)
+#define CPU_CSR_MTVEC (0x305)
+#define CPU_CSR_MEPC (0x341)
+#define CPU_CSR_MCAUSE (0x342)
+
 typedef struct {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   vaddr_t pc;
+  word_t csr[CPU_CSR_SIZE];
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
 // decode
--- src/isa/riscv32/system/intr.c
+++ src/isa/riscv32/system/intr.c
@@ -14,13 +14,17 @@
 ***************************************************************************************/
 
 #include <isa.h>
+#include "isa-def.h"
+
+extern void __am_asm_trap(void);
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-
-  return 0;
+  cpu.csr[CPU_CSR_MEPC] = epc;
+  cpu.csr[CPU_CSR_MCAUSE] = NO;
+  return (word_t) cpu.csr[CPU_CSR_MTVEC];
 }
 
 word_t isa_query_intr() {
